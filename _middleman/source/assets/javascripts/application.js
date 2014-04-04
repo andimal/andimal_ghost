67dd33a36071e47f3d8311d23cfc73fcda5b8cc2
Andy Richardson <andy@andimal.co>
2014-04-04 00:02:23 -0500

    woops, accidently ignored all assets. Added syntax highlighting and post styling

---
+++ _middleman/source/assets/javascripts/application.js
@@ -0,0 +1,5 @@
+//= require jquery
+//= require vendor/jquery.stellar
+//= require vendor/prettify
+
+//= require custom/header
---
+++ _middleman/source/assets/javascripts/custom/header.js.coffee
@@ -0,0 +1,5 @@
+$ ->
+  $.stellar()
+
+  $(window).load ->
+    prettyPrint()
